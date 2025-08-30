dc1a4d2a9c6674ecefcf992daa38c9eae48815fd
MohammadrezaNazirii <mohammadrezanaziri0@gmail.com>
2025-08-30 18:52:30 +0330

    Upload project

---
+++ images.py
@@ -0,0 +1,26 @@
+import fitz
+from io import BytesIO
+
+##################################################################
+#            3.  Extract Images    
+##################################################################
+
+
+def extract_images(uploaded_file):
+    uploaded_file.seek(0)
+    doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+    image_bytes_list = []
+
+    for page_num in range(len(doc)):
+        page = doc[page_num]
+        images = page.get_images(full=True)
+
+        for img_index, img in enumerate(images):
+            xref = img[0]
+            base_image = doc.extract_image(xref)
+            image_bytes = base_image["image"]
+            # store as BytesIO object
+            image_bytes_io = BytesIO(image_bytes)
+            image_bytes_list.append(image_bytes_io)
+
+    return image_bytes_list
---
+++ main.py
@@ -0,0 +1,57 @@
+import streamlit as st
+from title import extract_title_lines
+from abstract import extract_text_blocks, detect_document_language, extract_abstract_lines
+from images import extract_images
+from authors import extract_title_and_following_text, get_text_before_abstract_from_following_text
+from references import extract_references_from_pdf
+
+##################################################################
+#                Main Program    
+##################################################################
+
+labels = {
+    "title": {"en": "Title", "fa": "عنوان"},
+    "abstract": {"en": "Abstract", "fa": "چکیده"},
+    "authors": {"en": "Authors", "fa": "نویسنده ها"},
+    "images": {"en": "Images", "fa": "تصاویر"},
+    "upload": {"en": "Upload PDF", "fa": "بارگذاری PDF"},
+    "refs": {"en": "References", "fa": "منابع"}
+}
+
+st.title(labels["upload"]["en"])
+
+pdf_file = st.file_uploader(labels["upload"]["en"], type="pdf")
+
+if pdf_file:
+
+    text_blocks = extract_text_blocks(pdf_file)
+    language = detect_document_language(text_blocks)
+    abstract, page_number_ = extract_abstract_lines(pdf_file, language)
+    title = extract_title_lines(pdf_file)
+    images = extract_images(pdf_file)
+    result = extract_title_and_following_text(pdf_file, title, page_number_)
+    authors = get_text_before_abstract_from_following_text(result['text_after_title'], language)
+    title = result['matched_title']
+    refs = extract_references_from_pdf(pdf_file)
+
+    lang = language
+
+    # Display extracted information
+    st.header(labels["title"][lang])
+    st.write(title)
+
+    st.header(labels["authors"][lang])
+    st.write(authors)
+
+    st.header(labels["abstract"][lang])
+    st.write(abstract)
+
+    st.header(labels["refs"][lang])
+    st.text(refs)
+
+    st.header(labels["images"][lang])
+    if images:
+        for img in images:
+            st.image(img)
+    else:
+        st.write("No images found in PDF")
